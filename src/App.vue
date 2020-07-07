8f1a8909ce59b6ae0fb21766b7969865c1790e8b
Matt Jones <mattjonesorg@users.noreply.github.com>
2020-07-07 10:39:17 -0500

    Adding matt's microfrontend.
    step 3 from the workshop.

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
         <b-navbar-nav>
           <b-nav-item to="/">Home</b-nav-item>
           <b-nav-item to="/vince">Vince's Movie Recommendations</b-nav-item>
+          <b-nav-item to="/matt">Matt's micro-frontend</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
